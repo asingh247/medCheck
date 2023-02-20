6cf691c3a07203d96ba6b0b354737af4fdb77ef3
Anirudh Singh <anirudhsingh247@gmail.com>
2023-02-20 10:55:28 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1e13d160e903f0748f68520f1702b8e50d9610d946b518507586dbb6ea6ef272
+size 205
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:230f042899007b1fa24f503d520c88d634408a766c2383469a3194f80da5c63c
+size 4538
